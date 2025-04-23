905315d2a4c09079a7d8d731f76efefb77579d4b
rStar <starlove1247@gmail.com>
2025-04-23 11:33:42 +0800

    修改為InputSystem方式

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
@@ -8,16 +10,27 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 3f;
 
+    [SerializeField]
+    private PlayerInput playerInput;
+
+    private InputAction moveAction;
+
+    private void Start()
+    {
+        moveAction = playerInput.actions.FindAction("Move");
+    }
+
     // Update is called once per frame
     void Update()
     {
+        var moveVector2 = moveAction.ReadValue<Vector2>();
         // 水平的輸入 左右   -1 ~ 1
-        var horizontal = Input.GetAxisRaw("Horizontal");
+        // var horizontal = Input.GetAxisRaw("Horizontal");
         // 垂直的輸入 上下 -1 ~ 1
-        var vertical = Input.GetAxisRaw("Vertical");
+        // var vertical = Input.GetAxisRaw("Vertical");
         // 移動方向
-        var direction = new Vector3(horizontal , vertical , 0); // 1,1,0
-        direction = direction.normalized; // 正規化後 避免斜著走特別快的問題，最大為0.71f, 0.71f,0
+        var direction = new Vector3(moveVector2.x , moveVector2.y , 0); // 1,1,0
+        // direction = direction.normalized;                               // 正規化後 避免斜著走特別快的問題，最大為0.71f, 0.71f,0
         // 移動向量 deltaTime = 1/fps , 1/60 = 0.16667f
         var movement = direction * moveSpeed * Time.deltaTime;
         transform.position += movement;
