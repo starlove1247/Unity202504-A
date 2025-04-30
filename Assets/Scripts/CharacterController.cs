1ba6c1ced76b92118ee37a24ae76f29e621fe4dd
rStar <starlove1247@gmail.com>
2025-04-30 10:00:52 +0800

    讓角色GameObject變成Prefab

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -33,13 +33,8 @@ public class CharacterController : MonoBehaviour
         }
 
         var moveVector2 = moveAction.ReadValue<Vector2>();
-        // 水平的輸入 左右   -1 ~ 1
-        // var horizontal = Input.GetAxisRaw("Horizontal");
-        // 垂直的輸入 上下 -1 ~ 1
-        // var vertical = Input.GetAxisRaw("Vertical");
         // 移動方向
         var direction = new Vector3(moveVector2.x , moveVector2.y , 0); // 1,1,0
-        // direction = direction.normalized;                               // 正規化後 避免斜著走特別快的問題，最大為0.71f, 0.71f,0
         // 移動向量 deltaTime = 1/fps , 1/60 = 0.16667f
         var movement = direction * moveSpeed * Time.deltaTime;
         transform.position += movement;
