7cf7b83aeb61ddad67964a3b14cbaf3e49b4e7df
rStar <starlove1247@gmail.com>
2025-05-28 11:14:17 +0800

    實作
    NPC偵測到角色進入範圍後，顯示對話提示UI
    NPC偵測到角色離開範圍後，關閉對話提示UI

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using NaughtyAttributes;
 using UnityEngine;
 
@@ -9,6 +11,28 @@ public class NPC : MonoBehaviour
     [SerializeField]
     private Dialog dialog;
 
+    /// <summary>
+    /// NPC UI物件
+    /// </summary>
+    [SerializeField]
+    private CanvasGroup npcUI_Panel;
+
+    private void Start()
+    {
+        // 預設關閉對話視窗
+        dialog.gameObject.SetActive(false);
+        npcUI_Panel.alpha = 0; // 預設設定UI透明度為0
+    }
+    [Button("顯示NPC對話提示UI")]
+    private void ShowNPCDialogHintUI()
+    {
+        npcUI_Panel.DOFade(1 , 0.5f);
+    }
+    [Button("關閉NPC對話提示UI")]
+    private void CloseNPCDialogHintUI()
+    {
+        npcUI_Panel.DOFade(0 , 0.5f);
+    }
     [Button("開始對話")]
     public void StartDialog()
     {
@@ -20,5 +44,15 @@ public class NPC : MonoBehaviour
     public void PlayNextDialog()
     {
         dialog.PlayNextDialog();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        ShowNPCDialogHintUI();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        CloseNPCDialogHintUI();
     }
 }
